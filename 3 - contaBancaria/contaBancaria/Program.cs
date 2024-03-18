8f975df7fd0d6d0e25bba7f83eac3c52c0865bc0
philipheferreira <philipheferreira@gmail.com>
2024-03-18 11:24:52 -0300

    Update: project account bank

---
+++ 3 - contaBancaria/contaBancaria/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+class Programa{
+
+    static void Main(string[] args){
+
+        double saldoConta = 2600.14;
+        string nomeConta = "Philiphe Siqueira Ferreira";
+
+        Console.WriteLine("Nome: {0}", nomeConta);
+        Console.WriteLine("Saldo: {0}",saldoConta);
+    }
+
+}
