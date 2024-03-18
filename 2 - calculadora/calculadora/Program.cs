2db3eb1620f8ae412f9d07adcb0f88868ab9a021
philipheferreira <philipheferreira@gmail.com>
2024-03-18 10:45:17 -0300

    update: struct calculate

---
+++ 2 - calculadora/calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+class Programa{
+
+    static void Main(string[] args){
+
+        int soma, sub, mult, div;
+
+        int valor1 = 20, valor2 = 10;
+
+        soma = valor1 + valor2;
+        sub = valor1 - valor2;
+        mult = valor1 * valor2;
+        div = valor1 / valor2;
+
+        Console.WriteLine("A soma das variaveis {0} e {1} é {2}", valor1, valor2, soma);
+        Console.WriteLine("A subtração das variaveis {0} e {1} é {2}", valor1, valor2, sub);
+        Console.WriteLine("A multiplicação das variaveis {0} e {1} é {2}", valor1, valor2, mult);
+        Console.WriteLine("A divisão das variaveis {0} e {1} é {2}", valor1, valor2, div);
+
+    }
+
+}
