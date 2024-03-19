4d4637eaf622a14e6ce9045de1890d47f3f95863
philipheferreira <philipheferreira@gmail.com>
2024-03-19 11:37:16 -0300

    Update: menu ption

--- 1 - projetoarea/projetoarea/Program.cs
+++ 1 - projetoarea/projetoarea/Program.cs
@@ -4,26 +4,59 @@ class Programa{
 
     static void Main(String[] args){
 
-        int triangAlt = 20, triangBase = 10 , triangArea;
-
-        int quadLado = 8, quadArea;
+        int triangAlt = 20, triangBase = 10 , triangArea, quadLado = 8, quadArea, retLadoAlt = 5, retLadoBase = 10, retArea;
 
         double circArea, circRaio = 4;
 
+        int menu;
+
+        do {
+
+            Console.WriteLine("***Bem vindo ao menu de formas geometricas, digite qual area você quer calcular");
+            Console.WriteLine("1 - Quadrado");
+            Console.WriteLine("2 - Retangulo");
+            Console.WriteLine("3 - Triangulo");
+            Console.WriteLine("4 - Circulo");
+            Console.WriteLine("0 - Sair");
+            menu = Convert.ToInt32(Console.ReadLine());
+
+            switch (menu){
+
+                case 1:
+
+                    quadArea = quadLado * 4;
+
+                    Console.WriteLine("A área do quadrado é {0}", quadArea);
+
+                    break;
+                case 2:
+
+                    retArea = retLadoAlt * retLadoBase;
+
+                    Console.WriteLine("A area da altura e base do retangulo é {0}", retArea);
+
+                    break;
+                case 3:
+
+                    triangArea = triangAlt * triangBase;
+
+                    Console.WriteLine("A área do triangulo é {0}", triangArea);
+
+                    break;
+
+                case 4:
+
+                    circArea = circRaio * 3.14;
+
+                    Console.WriteLine("A área do circulo é {0} ", circArea);
+
+                    break;
+
+            }
+
+        } while (menu != 0);
 
 
-
-        triangArea = triangAlt * triangBase;
-
-        quadArea = quadLado * 4;
-
-        circArea =  circRaio * 3.14;
-
-        Console.WriteLine("A área do triangulo é {0}", triangArea);
-
-        Console.WriteLine("A área do quadrado é {0}", quadArea);
-
-        Console.WriteLine("A área do circulo é {0} ", circArea);
 
     }
 
--- 2 - calculadora/calculadora/Program.cs
+++ 2 - calculadora/calculadora/Program.cs
@@ -8,15 +8,44 @@ class Programa{
 
         int valor1 = 20, valor2 = 10;
 
-        soma = valor1 + valor2;
-        sub = valor1 - valor2;
-        mult = valor1 * valor2;
-        div = valor1 / valor2;
+        int menu;
 
-        Console.WriteLine("A soma das variaveis {0} e {1} é {2}", valor1, valor2, soma);
-        Console.WriteLine("A subtração das variaveis {0} e {1} é {2}", valor1, valor2, sub);
-        Console.WriteLine("A multiplicação das variaveis {0} e {1} é {2}", valor1, valor2, mult);
-        Console.WriteLine("A divisão das variaveis {0} e {1} é {2}", valor1, valor2, div);
+        do
+        {
+
+            Console.WriteLine("Ola, bem vindo a sua calculadora. Escolha o numero para selecionar sua operação");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("0 - Sair");
+            menu = Convert.ToInt32(Console.ReadLine());
+
+            switch (menu){
+                case 1:
+
+                    soma = valor1 + valor2;
+                    Console.WriteLine("A soma das variaveis {0} e {1} é {2}", valor1, valor2, soma);
+                    break;
+                case 2:
+
+                    sub = valor1 - valor2;
+                    Console.WriteLine("A subtração das variaveis {0} e {1} é {2}", valor1, valor2, sub);
+                    break;
+                case 3:
+
+                    mult = valor1 * valor2;
+                    Console.WriteLine("A multiplicação das variaveis {0} e {1} é {2}", valor1, valor2, mult);
+                    break;
+                case 4:
+
+                    div = valor1 / valor2;
+                    Console.WriteLine("A divisão das variaveis {0} e {1} é {2}", valor1, valor2, div);
+                    break;
+            }
+
+        } while (menu != 0);
+
 
     }
 
